72f511f014645ef084ffe88c3fbcff86b36dda59
Rabea Ahmed <52819954+ahmedrabea0124@users.noreply.github.com>
2021-05-28 00:59:18 +0600

    Add files via upload

---
+++ review.php
@@ -0,0 +1,24 @@
+<?php 
+
+
+
+  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<link rel="stylesheet" href="review.css">
+<body>
+	<div class="statusbox">
+		<div class="resigninas">	
+	<a href="postreview.php">Post Review</a>
+	<a href="checkreview.php">Check Review</a>
+</div>
+<div class="home">
+			<br><br><br>
+			<center><a href="home.php">Home</a></center>
+		</div>
+	</div>
+</body>
+</html>
---
+++ session.php
@@ -0,0 +1,14 @@
+<?php 
+
+session_start();
+include 'dbh.php';
+
+$username=$_SESSION['username'];
+
+// $sql="SELECT * FROM student WHERE username='$username'";
+// $result=$conn->query($sql);
+// $retrive=mysqli_fetch_array($result);
+
+
+
+ ?>
