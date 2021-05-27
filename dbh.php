d2940859be2b24cae073df33af832414ada2ef46
nraghib <81284860+nraghib@users.noreply.github.com>
2021-05-28 01:00:26 +0600

    Add files via upload

---
+++ dbh.php
@@ -0,0 +1,8 @@
+<?php
+// $conn=mysqli_connect("localhost","root","","onlineTuition");
+$conn=mysqli_connect("localhost","root","","knowledgehub");
+if(!$conn)
+	die("Connection Failed".mysqli_connect_error());
+
+
+?>
